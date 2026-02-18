1b7048b63dd4df20558482c08029ef49740623b3
navam-18 <navam.srivastava_cs22@gla.ac.in>
2026-02-18 18:49:14 +0530

    [Navam Srivastava].Add adding UC-1 Feet Equality

---
+++ src/test/java/com/bridgelabz/QuantityMeasurementAppTest.java
@@ -0,0 +1,39 @@
+package com.bridgelabz;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class QuantityMeasurementAppTest {
+
+    @Test
+    public void testEquality_SameValue() {
+        QuantityMeasurementApp.Feet f1 = new QuantityMeasurementApp.Feet(1.0);
+        QuantityMeasurementApp.Feet f2 = new QuantityMeasurementApp.Feet(1.0);
+        assertTrue(f1.equals(f2));
+    }
+
+    @Test
+    public void testEquality_DifferentValue() {
+        QuantityMeasurementApp.Feet f1 = new QuantityMeasurementApp.Feet(1.0);
+        QuantityMeasurementApp.Feet f2 = new QuantityMeasurementApp.Feet(2.0);
+        assertFalse(f1.equals(f2));
+    }
+
+    @Test
+    public void testEquality_NullComparison() {
+        QuantityMeasurementApp.Feet f1 = new QuantityMeasurementApp.Feet(1.0);
+        assertFalse(f1.equals(null));
+    }
+
+    @Test
+    public void testEquality_SameReference() {
+        QuantityMeasurementApp.Feet f1 = new QuantityMeasurementApp.Feet(1.0);
+        assertTrue(f1.equals(f1));
+    }
+
+    @Test
+    public void testEquality_NonNumericInput() {
+        QuantityMeasurementApp.Feet f1 = new QuantityMeasurementApp.Feet(1.0);
+        assertFalse(f1.equals("abc"));
+    }
+}
